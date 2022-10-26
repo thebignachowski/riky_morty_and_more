6ba4af55a321d6f6cc7365694b576a35dd26c146
Nacho Rodriguez <nacho.rodriguezbe@gmail.com>
2022-10-26 12:15:24 +0200

    Proyecto en React

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,24 @@
+import { GitItem } from './GitItem'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+
+export const GifGrid = ({ category }) => {
+
+    const { images, isLoading } = useFetchGifs( category )
+
+    return (
+        <>
+            <h3>{ category }</h3>
+
+            <div className='card-grid'>
+                {
+                    images.map( ( image ) => (
+                        <GitItem 
+                            key={ image.id }
+                            { ...image }
+                        />
+                    ))
+                }
+            </div>
+        </>
+    )
+}
---
+++ src/components/GitItem.jsx
@@ -0,0 +1,8 @@
+export const GitItem = ({ title, url, id }) => {
+    return (
+        <div className="card">
+            <img src={ url } alt={ title }></img>
+            <p>{ title }</p>
+        </div>
+    )
+}
